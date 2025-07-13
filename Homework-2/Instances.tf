9e897239ab334082300d169fe4f62ad4a42d3de1
mackpat3 <mackpat3@gmail.com>
2025-07-13 22:05:36 +0000

    added hmwk2 files

--- Homework-2/Instances.tf
+++ Homework-2/Instances.tf
@@ -1,9 +1,11 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "web1" {
   ami           = "ami-05f991c49d264708f"
   instance_type = "t2.micro"
   availability_zone = "us-west-2a"
   subnet_id = "subnet-07b4f457d97d9aa7c"
   key_name = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  user_data = file("apache.sh")
 
   tags = {
     Name = "web1"
@@ -16,6 +18,8 @@ resource "aws_instance" "web2" {
   availability_zone = "us-west-2b"
   subnet_id = "subnet-08ff7bccfd2bed827"
   key_name = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  user_data = file("apache.sh")
 
   tags = {
     Name = "web2"
@@ -28,9 +32,22 @@ resource "aws_instance" "web3" {
   availability_zone = "us-west-2c"
   subnet_id = "subnet-090dd60cee7424319"
   key_name = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  user_data = file("apache.sh")
 
   tags = {
     Name = "web3"
   }
 }
 
+output "web1" {
+  value = aws_instance.web1.public_ip
+}
+
+output "web2" {
+  value = aws_instance.web2.public_ip
+}
+
+output "web3" {
+  value = aws_instance.web3.public_ip
+}
